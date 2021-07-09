3dd0acff29f117d419ee9f8156ed8a6758af1201
thais <thais.abreuoliveira@hotmail.com>
2021-07-09 13:11:44 -0300

    Add project files.

---
+++ Controller/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuBancoLocalSQL.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetActionResult()
+        {
+            return new string[] { "value1", "value2", "value3", "value4" };
+        }
+
+    }
+
+}
